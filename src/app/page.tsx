82f2012443856ef0a0f8e434997466988bb53e3f
Kuben <jlawniczak@edu.cdv.pl>
2026-08-27 02:23:01 +0200

    Dodanie narzedzi analitycznych oraz narzedzi do mierzenia predkosci strony

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,6 +8,7 @@ import {ReviewsSection} from "@/components/features/ReviewSection";
 import {Pricing} from "@/components/features/Pricing";
 import {GallerySection} from "@/components/features/GallerySection";
 import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export default function Home() {
     return (
@@ -22,6 +23,7 @@ export default function Home() {
             <ContactSection />
             <GallerySection />
             <Analytics />
+            <SpeedInsights/>
         </main>
     );
 }
